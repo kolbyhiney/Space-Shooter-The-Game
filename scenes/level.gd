98b8644c046480d26fb568678db5b5f9e64a0abb
kolbyhiney <Squirrel2008!>
2026-04-19 09:53:15 +0300

    Upload before application

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+# 1. Load the scene
+var meteor_scene: PackedScene = load("res://scenes/meteor.tscn")
+var laser_scene: PackedScene = load("res://scenes/laser_scene.tscn")
+func _on_meteor_timer_timeout() -> void:
+	# 2. create an instance
+	var  meteor = meteor_scene.instantiate()
+	
+	# 3. attach the node to the scene tree
+	$Meteors.add_child(meteor)
 
-func _on_timer_timeout() -> void:
-	print("Meteorr")
+
+func _on_player_laser(pos) -> void:
+	var laser = laser_scene.instantiate()
+	$Lasers.add_child(laser)
+	laser.position = pos
